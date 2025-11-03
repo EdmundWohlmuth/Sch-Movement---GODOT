c44139462956b241133508c3d3ceec09d60bcadd
Edmund Wohlmuth <74564582+EdmundWohlmuth@users.noreply.github.com>
2025-11-03 11:25:15 -0400

    Testing

--- Weapon Resources/weapon_melee.gd
+++ Weapon Resources/weapon_melee.gd
@@ -1,5 +1,7 @@
 extends weapon_base
 
+@export var damage_box:String
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
   projectile_type = projectile_types.melee
