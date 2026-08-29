0ddf8279d6fa2d89328034937a9bd05bde3b1c80
Edmund Wohlmuth <74564582+EdmundWohlmuth@users.noreply.github.com>
2026-08-29 13:54:52 -0300

    nav mesh

--- Scripts/Enemies/enemy_behavior.gd
+++ Scripts/Enemies/enemy_behavior.gd
@@ -1,16 +1,23 @@
 extends CharacterBody3D
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+@export var speed:float = 5.0
+@export var rotation_speed:float = 5.0
+const JUMP_VELOCITY:float = 4.5
 
 @export var weapon_manager:weapon_node
 var current_weapon:weapon_base
 
 @export var weapon:weapon_node.weapons
 
+var has_target_pos:bool = true
+var target_pos
+
+@export var player:CharacterBody3D
+
 # ==== NODES ==== #
 @onready var health: health_node = $Health
 @onready var hurt_box: hurtbox_node = $hurt_box
+@onready var nav_agent: NavigationAgent3D = $NavigationAgent3D
 
 func _ready() -> void:
   weapon_manager.set_weapon(weapon)
@@ -23,15 +30,21 @@ func _physics_process(delta: float) -> void:
   # Handle jump.
   #if Input.is_action_just_pressed("ui_accept") and is_on_floor():
     #velocity.y = JUMP_VELOCITY
+    
+  if has_target_pos:
+      # Move to target position
+      nav_agent.target_position = player.position
+      var path_pos = nav_agent.get_next_path_position()
+      var dir = global_position.direction_to(path_pos)
+      
+      velocity = dir * speed
+      
+      # rotate towards movement
+      var rotate_towards = dir.signed_angle_to(Vector3.MODEL_FRONT, Vector3.DOWN)
+      rotation.y = move_toward(rotation.y, rotate_towards, delta * rotation_speed)
 
-
-  #var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-  #var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-  #if direction:
-    #velocity.x = direction.x * SPEED
-    #velocity.z = direction.z * SPEED
-  #else:
-    #velocity.x = move_toward(velocity.x, 0, SPEED)
-    #velocity.z = move_toward(velocity.z, 0, SPEED)
-
+  #move_and_slide()
+  
+func _on_navigation_agent_3d_velocity_computed(safe_velocity: Vector3) -> void:
+  velocity = velocity.move_toward(safe_velocity, 1)
   move_and_slide()
